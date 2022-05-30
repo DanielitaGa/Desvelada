5584a99550617fc5eea3571ac06a5960e73e07b0
DanielitaGa <danielagarciache1999@gmail.com>
2022-05-30 01:44:30 -0500

    primer commit

---
+++ app/src/main/java/com/nadershamma/apps/mymenus/MainActivity.java
@@ -0,0 +1,37 @@
+package com.nadershamma.apps.mymenus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuprincipal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.ItemPrimero:
+                Toast.makeText(this,"Abriste el itemprimero",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.ItemSegundo:
+                Toast.makeText(this,"Abriste itemsegundo", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
